07cc2ccec77b1edb419e11d514cf422265e2285b
YUVRAJ K V <yuvrajkv505@gmail.com>
2023-07-12 20:35:28 +0530

    adding assignment program file

---
+++ AmazonPrime.java
@@ -0,0 +1,65 @@
+class AmazonPrime{
+
+  static String kannada[]={"kaanthara","rajakumara","robert","gultu","kirikparty"};
+  static String telugu[]={"sir","dasara","virupaksha","hit","custody"};
+  static String hindi[]={"bhaagi","ashique","chichore","pathan","hit"};
+  static String tamil[]={"beast","varisu","valimai","master","vikram"};
+  static String english[]={"avengers","warrior","thegodfather","fall","matrix"};
+  static String malayalam[]={"hridayam","kurup","premam","drishyam","oppam"};
+  
+  public static void main(String[] args){
+  
+  System.out.println(kannada.length);
+  
+  for(int kannadaIndex=0 ;kannadaIndex < kannada.length;kannadaIndex++){
+	  System.out.println(kannada[kannadaIndex]);
+  }
+  
+  System.out.println(telugu.length);
+  
+  for(int teluguIndex=0 ;teluguIndex < telugu.length;teluguIndex++){
+	  System.out.println(telugu[teluguIndex]);
+  }
+  
+  System.out.println=(hindi.length);
+  for(int hindiIndex=0 ;hindiIndex < hindi.length;hindiIndex++){
+	  System.out.println(hindi[hindiIndex]);
+  }
+  for(int tamilIndex=0 ;tamilIndex < tamil.length;tamilIndex++){
+	  System.out.println(tamil[tamilIndex]);
+  }
+  
+  for(int englishIndex=0 ;englishIndex < english.length;englishIndex++){
+	  System.out.println(english[englishIndex]);
+  
+  }
+  
+  for(int malayalamIndex=0 ;malayalamIndex < malayalam.length;malayalamIndex++){
+	  System.out.println(malayalam[malayalamIndex]);
+  }
+  
+  
+  
+  
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+}
+
---
+++ Bikemount.java
@@ -0,0 +1,32 @@
+class Bikemount{
+
+static String name = "Otoroys Universal Bike Mount Holder";
+static String brand = "otoroys";
+static String colour = "black";
+static String material = "aluminium";
+static String mountedOn = "bike, scooty";
+static String mountingType = "handle fitting";
+static double maximumWidthInCm = 10.5;
+static double maximumLengthInCm = 7.5;
+static int price = 272;
+
+
+
+public static void main(String[] value){
+
+
+System.out.println("name:"+name);
+System.out.println("brand:"+brand);
+System.out.println("colour:"+colour);
+System.out.println("material:"+material);
+System.out.println("mountedOn:"+mountedOn);
+System.out.println("mountingType:"+mountingType);
+System.out.println("maximumWidthInCm:"+maximumWidthInCm);
+System.out.println("maximumLengthInCm:"+maximumLengthInCm);
+System.out.println("price:"+price);
+
+
+}
+
+
+}
