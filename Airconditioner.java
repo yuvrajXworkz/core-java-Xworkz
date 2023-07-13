856d901e4c0d313e24379d038c7b22557030275d
yuvrajXworkz <yuvrajkv505@gmail.com>
2023-07-13 13:36:57 +0530

    uploading files

---
+++ Airconditioner.java
@@ -0,0 +1,34 @@
+class Airconditioner{
+
+static String name = "Star Split Inverter AC";
+static String brand ="voltas";
+static String modelName = "Vectra Pride";
+static String type = "split";
+static float capacityInTons = 1.5f;
+static String colour = "white";
+static int cooloingCapacity = 1500;
+static String compressor = "High EER Rotary- BLDC";
+static int price = 35000;
+
+
+public static void main(String[] voltas){
+
+System.out.println("name:"+name);
+System.out.println("brand:"+brand);
+System.out.println("modelName:"+modelName);
+System.out.println("type:"+type);
+System.out.println("capacityInTons:"+capacityInTons);
+System.out.println("colour:"+colour);
+System.out.println("cooloingCapacity:"+cooloingCapacity);
+System.out.println("compressor:"+compressor);
+System.out.println("price:"+price);
+
+
+
+
+
+
+}
+
+
+}
