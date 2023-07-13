0957b36feef11418733272beb92f954e2ecc972d
yuvrajXworkz <yuvrajkv505@gmail.com>
2023-07-13 13:59:47 +0530

    uplading files

---
+++ LapTop.java
@@ -0,0 +1,19 @@
+class LapTop{
+  public static void main(String[]args){
+  
+  String brand="lenevo";
+  System.out.println("brand:"+brand);
+  
+  int price=62000;
+  System.out.println("price:"+price);
+  
+  String processor="amdryzen";
+  System.out.println("processor:"+processor);
+  
+  float screensize=15.6f;
+  System.out.println("screensize:"+screensize);
+  
+  String colour="block";
+  System.out.println("colour:"+colour);
+  }
+}
---
+++ LuminousRedCharge.java
@@ -0,0 +1,20 @@
+class LuminousRedCharge{
+  
+  public static void main(String[] shorttubularplateinverterbattery){
+    
+    String brand="Luminous";
+    int price=11659;
+    String powerSource="BatteryPowered";
+    String modelName="RC18000ST 150 AH BATTERY";
+    String colour="Blue,White";
+    String includedComponents="1 Inverter Battery";
+    
+    
+    System.out.println("brand:"+brand);
+    System.out.println("price:"+price);
+    System.out.println(" powerSource:"+ powerSource);
+    System.out.println(" modelName:"+ modelName);
+    System.out.println(" colour:"+ colour);
+    System.out.println(" includedComponents:"+ includedComponents);
+  }
+}
