e3d226852110e6575a3fce397b10799f36146817
yuvrajXworkz <yuvrajkv505@gmail.com>
2023-07-13 14:10:23 +0530

    uploading file

---
+++ ToyataFortuner.java
@@ -0,0 +1,32 @@
+class ToyataFortuner{
+  
+  public static void main(String[] esha)
+  {
+    
+    String name = "Toyata";
+    String fuelType = "Diesel";
+    int engineDisplacement = 2755;
+    int noOfCylinder = 4;
+    String power = "3400rpm";
+    String torque = "1600-2800rpm";
+    int seatCapacity = 7;
+    String transmissionType = "Automation";
+    String bodyType = "SUV";
+    double price = 200000;
+    
+    System.out.println("name:"+name);
+    System.out.println("fuelType:"+fuelType);
+    System.out.println("engineDisplacement:"+engineDisplacement);
+    System.out.println("noOfCylinder:"+noOfCylinder);
+    System.out.println(" power :"+ power );
+    System.out.println("torque:"+torque);
+    System.out.println(" seatCapacity :"+ seatCapacity );
+    System.out.println("bodyType:"+bodyType);
+    System.out.println("price:"+price);
+    
+    
+  }
+  
+  
+  
+}
