c4bd88eabaa261b16bb51ccf1aca0fb6a84ff6f5
yuvrajXworkz <yuvrajkv505@gmail.com>
2023-07-13 13:50:55 +0530

    uploading files

---
+++ Dell.java
@@ -0,0 +1,35 @@
+class Dell {
+  
+  public static void main(String[] esha)
+  {
+    
+    String name = "Dell";
+    double depth = 23.5;
+    double weight = 1.8;
+    String ram = "8GB";
+    String operatingSystem = "Windows11";
+    int usb = 3;
+    int hdmiPort = 1;
+    String screenSize = "15.6inch";
+    String screenRevolution = "1920-1080Pixel";
+    String screenType = "FDH";
+    int speaker = 2;
+    double dimention = 23.5*35.8*1.9;
+    int price = 51000;
+    System.out.println("name:"+name);
+    System.out.println("depth:"+depth);
+    System.out.println("weight:"+weight);
+    System.out.println("ram:"+ram);
+    System.out.println("operatingSystem:"+operatingSystem);
+    System.out.println("usb:"+usb);
+    System.out.println(" hdmiPort:"+ hdmiPort);
+    System.out.println("screenSize:"+screenSize);
+    System.out.println("screenRevolution:"+screenRevolution);
+    System.out.println("screenType:"+screenType);
+    System.out.println("speaker:"+speaker);
+    System.out.println("dimention :"+dimention );
+    System.out.println("price:"+price);
+    
+  }
+  
+}
